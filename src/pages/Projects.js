65d0ae5448530311eb82b7d642899f902e89d211
Jesús Quiñones <1930318@upv.edu.mx>
2023-07-30 12:47:27 -0600

    Project item component and assests was added included his style

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import ProjectItem from '../components/ProjectItem'
+import employeeSystem from "../assets/employeeSystem.png";
+import bookCollection from "../assets/bookCollection.png";
+import "../styles/Projects.css";
 
 function Projects() {
   return (
     <div className='projects'>
       <h1> Mis proyectos personales </h1>
-      <div className="projectList"></div>
+      <div className="projectList">
+        <ProjectItem name="Sistema de empleados" image={employeeSystem} />
+        <ProjectItem name="Sistema de libros" image={bookCollection} />
+      </div>
     </div>
   )
 }
