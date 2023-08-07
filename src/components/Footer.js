04a87a685604ab5f16eca3af2512deb51b1ea2cb
Jesús Quiñones <1930318@upv.edu.mx>
2023-08-06 22:32:42 -0600

    Social Media Redirection was improved

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,10 +10,18 @@ function Footer() {
   return (
     <div className='footer'>
         <div className='socialMedia'>
-            <InstagramIcon href="www.google.com" />
+            <a href="https://www.instagram.com/Yisuslalala">
+              <InstagramIcon />
+            </a>
+            <a href="https://twitter.com/Yisuslalala">
             <TwitterIcon />
+            </a>
+            <a href="https://www.youtube.com/channel/UCZ3Q1amj-UR83oBNNwn7rkA">
             <YouTubeIcon />
+            </a>
+            <a href="https://www.linkedin.com/in/jes%C3%BAs-qui%C3%B1ones/">
             <LinkedInIcon />
+            </a>
         </div>
         <p> &copy; 2023 yisuslalala.com</p>
         </div>
